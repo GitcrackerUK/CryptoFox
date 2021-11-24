c39423f9a21647422abba9403d6f262dff362ddb
GitcrackerUK <p.f.siwek@gmail.com>
2021-11-24 21:25:51 +0000

    new route "getPrices"

--- server.js
+++ server.js
@@ -23,6 +23,34 @@ app.get('/getStats', function (req, res) {
 })
 
 
+app.get('/getPrices', function (req, res) {
+
+  const requestOptions = {
+    method: 'GET',
+    url: `${CRYPTO_PRICE_URL}`,
+    qs: {
+      'start': '1',
+      'limit': '5000',
+      'convert': 'USD'
+    },
+    headers: {
+      'X-CMC_PRO_API_KEY': `${CRYPTO_PRICE_KEY}`
+    },
+    json: true,
+    gzip: true
+  };
+
+  axios(requestOptions)
+  .then(response => {
+    console.log('API call response:', response.data);
+  })
+  .catch((err) => {
+    console.log('API call error:', err.message);
+  });
+
+
+})
+
 app.listen(Port,()=>{
     console.log(`App listen on ---> http://localhost:${Port}`)
 })
